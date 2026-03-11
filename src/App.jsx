7059904bf4924e33915ece193948ea365a9003f2
VOstroverkh <veronikaostroverkh@gmail.com>
2026-03-11 12:08:57 +0100

    ADD: footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css';
 import Header from './components/header';
 import Main from './components/main';
+import Footer from './components/footer';
 
 function App() {
   return (
-    <>
+    <div className="mainContainer">
       <Header />
       <Main />
-    </>
+      <Footer />
+    </div>
   );
 }
 
--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,10 +1,11 @@
 import NoteForm from '../noteForm';
 import React from 'react';
 import NoteList from '../noteList';
+import styles from './styles.module.css';
 
 function Main() {
   return (
-    <div>
+    <div className={styles.main}>
       <NoteForm />
       <NoteList />
     </div>
